42dd33c543188aa77110043d646df14827dc719c
kvijaya-datascientist <kvijaya.datascientist@gmail.com>
2025-09-02 21:39:07 +0530

    adding files for search agent using react agent architecture

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # langchain-practice
 This repository created for LangChain Practice 
+1. Initialize the project with below command in terminal
+       uv init
+2. add requried libraries
+langchain, python-dotenv, black, isort, langchain-openai, langchain-tavily
+
+3. for below error
+
+NotOpenSSLWarning: urllib3 v2 only supports OpenSSL 1.1.1+, currently the 'ssl' module is compiled with 'LibreSSL 2.8.3'.
+
+uv pip install "urllib3 < 2.0>"
+
+
---
+++ main.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from langchain.agents.react.agent import create_react_agent
+from langchain.agents import AgentExecutor
+from langchain import hub
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4", temperature=0)
+react_prompt = hub.pull("hwchase17/react")
+
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+def main():
+    #print("Hello from langchain-practice!")
+    result = chain.invoke(
+        input={
+            "input" : "please provide summary about vijaya lakshmi kaja and she is a software craftmen."
+        }
+    )
+    print(result)
+
+if __name__ == "__main__":
+    main()
