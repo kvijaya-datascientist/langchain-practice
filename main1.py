7a0150a61805ed5a71e2c125e912c52f5d274b0b
kvijaya-datascientist <kvijaya.datascientist@gmail.com>
2025-09-04 17:26:34 +0530

    adding files

--- main1.py
+++ main1.py
@@ -10,33 +10,22 @@ from langchain.agents.react.agent import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableLambda
 
-from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
-from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4", temperature=0)
 react_prompt = hub.pull("hwchase17/react")
 
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
-react_prompt_with_format_instructions = PromptTemplate(
-    template = REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables= ["input", "agent_scratchpad", "tool_names"]
-).partial(format_instructions=output_parser.get_format_instructions())
 
-agent = create_react_agent(
-    llm=llm,
-    tools=tools, 
-    prompt=react_prompt_with_format_instructions
-    )
+
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
+
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 chain = agent_executor
 
 
 def main():
+    # print("Hello from langchain-practice!")
     result = chain.invoke(
         input={
             "input": "please provide summary about vijaya lakshmi kaja and she is a software craftmen."
